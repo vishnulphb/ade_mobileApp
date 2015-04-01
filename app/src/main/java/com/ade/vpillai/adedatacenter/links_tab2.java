2a103eeb0105a9b8b514d7a0984a1d190fe45c40
Vishnu Pillai <vishnupillai02@gmail.com>
2015-04-01 12:22:35 -0500

    added Links Page, has a sliding navigation for two tabs, added animation

---
+++ app/src/main/java/com/ade/vpillai/adedatacenter/links_tab2.java
@@ -0,0 +1,20 @@
+package com.ade.vpillai.adedatacenter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vpillai on 4/1/2015.
+ */
+public class links_tab2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.links_tab2,container,false);
+        return v;
+    }
+}
